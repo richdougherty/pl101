821bb217a71ab972fd3b138e02eed7900ba7aecb
Rich Dougherty <rich@rd.gen.nz>
2012-04-27 17:03:10 +1200

    Added more tests for basic parser

--- scheem/scheem.js
+++ scheem/scheem.js
@@ -1,12 +1,22 @@
 var PEG = require('pegjs');
+var deepEqual = require('deep-equal');
 var assert = require('assert');
 var fs = require('fs'); // for loading files
 
 // Read file contents
 var data = fs.readFileSync('scheem.peg', 'utf-8');
-// Show the PEG grammar file
-console.log(data);
 // Create my parser
 var parse = PEG.buildParser(data).parse;
 // Do a test
-assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+
+function testParse(inputText, expectedAst) {
+	var ast = parse(inputText);
+	if (!deepEqual(ast, expectedAst)) {
+		console.log("ASTs do not match: ", ast, expectedAst);
+		assert.fail(ast, expectedAst);
+	}
+}
+
+testParse("(a b c)", ["a", "b", "c"]);
+testParse("(+ 1 (* x 3))", ["+", "1", ["*", "x", "3"]]);
+testParse("(* n (factorial (- n 1)))", ["*", "n", ["factorial", ["-", "n", "1"]]]);
